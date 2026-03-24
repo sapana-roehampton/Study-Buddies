c312afbbb091e2488980e2b96f0f172fc08c0322
sudippaudel111 <sudippaudyal11@gmail.com>
2026-03-24 09:43:03 +0000

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: "root",
   password: "root",
   database: "studybuddy",
-  waitForConnections: true,
+  waitforconnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
